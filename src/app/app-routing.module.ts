b16d25051fac2272266f55f51d352f2f9877e9ce
RDUBPhoto <rdubphoto@gmail.com>
2025-02-13 12:18:47 -0600

    Updates to packages, scrolling and bugs found. Dates and new roles updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ExtraOptions } from '@angular/router';
 import { HomeOneComponent } from './components/pages/home-one/home-one.component';
 import { WorksDetailsComponent } from './components/pages/works-details/works-details.component';
 
 const routes: Routes = [
-    {path: '', component: HomeOneComponent},
-    {path: 'works-details/:projectId', component: WorksDetailsComponent},
+    { path: '', component: HomeOneComponent },
+    { path: 'works-details/:projectId', component: WorksDetailsComponent },
 ];
 
+const routerOptions: ExtraOptions = {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+    scrollOffset: [0, 70]
+};
+
 @NgModule({
-    imports: [RouterModule.forRoot(routes, {})],
+    imports: [RouterModule.forRoot(routes, routerOptions)],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
